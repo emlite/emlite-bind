28b12484be517ed3144afe355dbce9c91e580922
Mohammed Alyousef <may642_2000@hotmail.com>
2025-07-21 13:35:47 +0300

    add audio example

--- jsbind/examples/promise.rs
+++ jsbind/examples/promise.rs
@@ -20,8 +20,14 @@ fn main() {
     println!("Promise.all result: {:?}", all);
 
     // Chaining with then/catch/finally using JS function bodies
-    let on_fulfilled = Function::new(&["value"], "console.log('Promise fulfilled with:', value); return 'ok';");
-    let on_rejected = Function::new(&["reason"], "console.log('Promise rejected with:', reason); return 'fail';");
+    let on_fulfilled = Function::new(
+        &["value"],
+        "console.log('Promise fulfilled with:', value); return 'ok';",
+    );
+    let on_rejected = Function::new(
+        &["reason"],
+        "console.log('Promise rejected with:', reason); return 'fail';",
+    );
     let chained = promise.then(&on_fulfilled, &on_rejected);
     let caught = chained.catch(&on_rejected);
     let finalized = caught.finally(&on_fulfilled);
---
+++ webbind/examples/audio.rs
@@ -0,0 +1,38 @@
+use jsbind::prelude::*;
+use webbind::audio_param::AudioParam;
+use webbind::html_button_element::HTMLButtonElement;
+use webbind::node::Node;
+use webbind::pointer_event::PointerEvent;
+use webbind::window;
+use webbind::{audio_context::AudioContext, enums::OscillatorType};
+
+fn main() {
+    let context = AudioContext::new0();
+    println!("Got an AudioContext");
+
+    // Create oscillator
+    let mut oscillator = context.create_oscillator();
+    println!("Configuring oscillator");
+    oscillator.set_type_(&OscillatorType::TRIANGLE);
+    oscillator.frequency().set_value(261.63); // Middle C
+
+    let document = window().document();
+    let body = document.get_elements_by_tag_name("body").item(0);
+    let mut button = document
+        .create_element0("BUTTON")
+        .dyn_into::<HTMLButtonElement>()
+        .unwrap();
+
+    button.set_text_content("Click me");
+    button.add_event_listener0(
+        "click",
+        &Closure::bind1(move |_p: PointerEvent| {
+            println!("Playing");
+            oscillator.connect0(context.destination().unchecked_ref::<AudioParam>());
+            oscillator.start1(0.0);
+            println!("All done!");
+        })
+        .into(),
+    );
+    body.append_child(button.dyn_ref::<Node>().unwrap());
+}
