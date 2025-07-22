3026dc4117cf88b2f4f14914f9c51db1d12e8ddf
Mohammed Alyousef <may642_2000@hotmail.com>
2025-07-22 12:30:08 +0300

    example

--- jsbind/examples/domstring.rs
+++ jsbind/examples/domstring.rs
@@ -2,5 +2,6 @@ use jsbind::prelude::*;
 
 fn main() {
     let s: DOMString = "Hello".into();
-    Console::get().log(&[s.into()]);
+    Console::get().log(&[s.clone().into()]);
+    println!("{}", s.as_str());
 }
